9bd13491f91dd47f6329567a4e01d3c092d15758
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2020-11-12 09:51:48 +0100

    add shellcheck when building
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- build_box.sh
+++ build_box.sh
@@ -2,6 +2,8 @@
 
 packer validate ubuntu-hardened-20.04-packer.json || exit 1
 vagrant validate Vagrantfile || exit 1
+shellcheck -x -s bash -f gcc scripts/* || exit 1
+
 vagrant destroy
 vagrant box remove ubuntu-hardened/20.04 --all
 rm -rvf ./output
