d6f182ffa8bcdade4459590972c2c5d14be3db20
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2020-11-19 18:29:17 +0100

    correct name in manifest file
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- scripts/postproc.sh
+++ scripts/postproc.sh
@@ -6,6 +6,8 @@ rmdir ./build
 cd ./output || exit 1
 
 find . -name "packer-*" -exec sh -c 'mv -v "$1" "$(echo "$1" | sed s/packer-//g)"' _ {} \;
+find . -name "*.mf" -exec sed -i.bak 's/packer-ubuntu-/ubuntu-/g' {} \;
+find . -name "*.bak" -exec rm -rvf {} \;
 find . -type f ! -name "*.sha256" -exec sha256sum {} \; > "${PACKER_BUILD_NAME}.sha256"
 
 cd .. || exit 1
