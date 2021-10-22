a0bb329aed21e0356145393be9dfc245e7626670
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2021-10-22 18:37:31 +0200

    ensure / is wiped, not only /tmp
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- scripts/minimize.sh
+++ scripts/minimize.sh
@@ -7,6 +7,10 @@ export HISTFILESIZE=0
 
 count=$(df --sync -kP / | tail -n1  | awk -F ' ' '{print $4}')
 count=$((count-1))
+
+dd if=/dev/zero of=/whitespace bs=1M count=$count || echo "dd exit code $? is suppressed";
+rm /whitespace
+
 dd if=/dev/zero of=/tmp/whitespace bs=1M count=$count || echo "dd exit code $? is suppressed";
 rm /tmp/whitespace
 
