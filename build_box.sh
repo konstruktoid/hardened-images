e409eda4eebac55911dc9ce522dac4660155408e
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2022-03-16 23:05:29 +0100

    update build_script with new file names
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- build_box.sh
+++ build_box.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -eux
 
-find . -name '*packer.json' -type f -exec packer validate {} \; || exit 1
+find . -name '*packer.pkr.hcl' -type f -exec packer validate {} \; || exit 1
 vagrant validate Vagrantfile || exit 1
 shellcheck -x -s bash -f gcc scripts/* || exit 1
 
-vagrant destroy --force
+vagrant destroy --force hardened
 vagrant box remove ubuntu-hardened/20.04 --all
 
 rm -rvf ./output
 
-packer build -force -timestamp-ui ubuntu-20.04-hardened-packer.json || exit 1
+packer build -force -timestamp-ui ubuntu-20.04-hardened-packer.pkr.hcl || exit 1
