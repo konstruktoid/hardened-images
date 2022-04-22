f31ac9f283101c0311e0ef916cfef79a36808a89
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2022-04-22 23:44:33 +0200

    use var file when building
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- build_box.sh
+++ build_box.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eux -o pipefail
 
-find . -name '*packer.pkr.hcl' -type f -exec packer validate {} \; || exit 1
+find . -name '*packer.pkr.hcl' -type f -exec packer validate -var-file ubuntu-20.04-vars.json {} \; || exit 1
 vagrant validate Vagrantfile || exit 1
 shellcheck -x -s bash -f gcc scripts/* || exit 1
 
