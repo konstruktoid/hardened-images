2659d70e3fe18f21761689ea8ddf4f7a5f7c15d1
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2023-10-10 12:26:12 +0000

    set AllowUsers vagrant if the user exists
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- scripts/hardening.sh
+++ scripts/hardening.sh
@@ -14,13 +14,12 @@ ansible-playbook -i '127.0.0.1,' -c local ./local.yml
 
 ufw disable;
 
-sed -i 's/AllowUsers.*/AllowUsers vagrant/g' /etc/ssh/sshd_config.d/01-hardening.conf
-
 systemctl restart sshd
 
 find /etc -name '*.bak' -exec rm -f {} \;
 
 if id vagrant; then
+  sed -i 's/AllowUsers.*/AllowUsers vagrant/g' /etc/ssh/sshd_config.d/01-hardening.conf
   chage --maxdays 365 vagrant
   chage --mindays 1 vagrant
 fi
