e74197686f9c64a5b08e2ccd33b12d07dfd9f3bd
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2024-04-29 20:09:30 +0000

    only look for 24.04 and rename box afterwards
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- build_box.sh
+++ build_box.sh
@@ -7,7 +7,6 @@ BASE_DIR="$(pwd)"
 GIT_CLONE_DIR="$(mktemp --directory -p /var/tmp bento.XXXXXX)"
 
 mkdir -p "${BASE_DIR}/output"
-rm -rvf "${BASE_DIR}/output/*.box"
 
 git clone https://github.com/chef/bento.git "${GIT_CLONE_DIR}"
 
@@ -20,14 +19,14 @@ git apply ./packer_templates/config/bento.diff
 
 packer init -upgrade ./packer_templates
 
-find . -name 'ubuntu-2[2-8].04-x86_64.pkrvars.hcl' | while read -r template; do
-  packer build -only=virtualbox-iso.vm -var-file="${template}" ./packer_templates
+find . -name 'ubuntu-2[4-8].04-x86_64.pkrvars.hcl' | while read -r template; do
+  packer build -only=virtualbox-iso.vm,vmware-iso.vm -var-file="${template}" ./packer_templates
   box_name="$(basename "${template}" | awk -F '-' '{print $2}')"
-  find . -name "${box_name}-*.box" | while read -r box; do
-    mv "${box}" "${BASE_DIR}/output/"
+  find . -name "ubuntu-${box_name}-*" | while read -r box; do
+    mod_name="$(basename "$box" | sed 's/virtualbox/bento-hardened/g')"
+    mv -v "${box}" "${BASE_DIR}/output/${mod_name}"
   done
 done
 
 cd "${BASE_DIR}"
-echo "${GIT_CLONE_DIR}"
-# rm -rf "${GIT_CLONE_DIR}"
+rm -rf "${GIT_CLONE_DIR}"
