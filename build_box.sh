2f26ad25b3d69379454b1c6c7e51d6d1c1d4606f
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2026-07-02 20:40:04 +0000

    bump to 26.04
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- build_box.sh
+++ build_box.sh
@@ -19,7 +19,7 @@ cd "${GIT_CLONE_DIR}" || exit 1
 git apply ./packer_templates/config/bento.diff
 
 packer init -upgrade ./packer_templates
-find . -name 'ubuntu-2[4-8].*-x86_64.pkrvars.hcl' | while read -r template; do
+find . -name 'ubuntu-26.*-x86_64.pkrvars.hcl' | while read -r template; do
   packer build -only="${BUILD_ISOS}" -var-file="${template}" ./packer_templates
   box_name="$(basename "${template}" | awk -F '-' '{print $2}')"
   find . -name "ubuntu-${box_name}-*.box" | while read -r box; do
